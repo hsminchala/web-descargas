44ee01f91c7124760a0a3635f9e8e256625706e9
hsminchala <83968950+hsminchala@users.noreply.github.com>
2022-01-16 22:18:03 -0500

    Update UsersService.kt

--- src/main/kotlin/com/sebastian/web/service/UsersService.kt
+++ src/main/kotlin/com/sebastian/web/service/UsersService.kt
@@ -22,9 +22,9 @@ class UsersService {
             val response = usersRepository.findByUsername(username)
                 ?: throw Exception("No existe el usuario")
             return response
-        } catch (ex:Exception){
+        } catch (ex: Exception){
             throw ResponseStatusException(
-                HttpStatus.NOT_FOUND, "User not found", ex)
+                HttpStatus.NOT_FOUND, ex.message, ex)
         }
     }
 
@@ -36,4 +36,60 @@ class UsersService {
         usersRepository.deleteById(id)
         return true
     }
+
+    //metodos de la logica
+    fun calcMultiplicationTest(index: Int, number:Int):Int{
+    if (index % 2 == 0){
+    return number * 2;
+    }
+        return number * 1
+    }
+
+    fun restNine(num:Int):Int{
+    if (num in 10..18){
+        return num - 9
+        }
+        return num
+    }
+
+    fun substractFromNextTen(ins:Int):Int{
+            var igg:Int = (ins/10) + 1
+            var  tis = igg * 10
+        return tis - ins
+    }
+
+
+    fun validarCedula(x: String):Boolean {
+        var suma = 0
+        return if (x.length == 9) {
+            println("Ingrese su cedula de 10 digitos")
+            false
+        } else {
+            val a = IntArray(x.length / 2)
+            val b = IntArray(x.length / 2)
+            var c = 0
+            var d = 1
+            for (i in 0 until x.length / 2) {
+                a[i] = x[c].toString().toInt()
+                c += 2
+                if (i < x.length / 2 - 1) {
+                    b[i] = x[d].toString().toInt()
+                    d += 2
+                }
+            }
+            for (i in a.indices) {
+                a[i] = a[i] * 2
+                if (a[i] > 9) {
+                    a[i] = a[i] - 9
+                }
+                suma = suma + a[i] + b[i]
+            }
+            val aux = suma / 10
+            val dec = (aux + 1) * 10
+            if (dec - suma == x[x.length - 1].toString()
+                    .toInt()
+            ) true else suma % 10 == 0 && x[x.length - 1] == '0'
+        }
+    }
+
 }
