0e1febf83e807301d217059701931755c1e68b7e
hsminchala <83968950+hsminchala@users.noreply.github.com>
2022-01-16 22:16:36 -0500

    Update WebApplicationTests.kt

--- src/test/kotlin/com/sebastian/web/WebApplicationTests.kt
+++ src/test/kotlin/com/sebastian/web/WebApplicationTests.kt
@@ -1,13 +1,30 @@
 package com.sebastian.web
 
+import com.sebastian.web.service.ProgramaService
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class WebApplicationTests {
 
+	@Autowired
+	lateinit var programaService: ProgramaService
+
 	@Test
 	fun contextLoads() {
+
+	}
+	@Test
+	fun verifySizeWordWhenIsIncorrect(){
+		val response: Boolean = programaService.verifyWord("J")
+		Assertions.assertEquals(false,response)
 	}
 
+	@Test
+	fun verifySizeWordWhenIsCorrect(){
+		val response: Boolean = programaService.verifyWord("Jajaja... te cache hermanito")
+		Assertions.assertEquals(true,response)
+	}
 }
